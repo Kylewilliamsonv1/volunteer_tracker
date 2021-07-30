1903f3c1ce157e5584470f76ba4ae0ab05003855
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-30 12:27:46 -0700

    volunteer.compare test; fail/pass

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,9 +1,31 @@
 class Volunteer
-  attr_accessor :name
+  attr_accessor :name, :project_id
   attr_reader :id
 
   def initialize(attributes)
-    @name = attributes.fetch(:name)
-    @id = attributes.fetch(:id) 
+    @name = attributes.fetch(:name) 
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
   end
+
+  def ==(volunteer_to_compare)
+    self.name() == volunteer_to_compare.name()
+  end
+
+  # def self.all
+  #   returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+  #   volunteers = []
+  #   returned_volunteers.each() do |volunteer|
+  #     name = volunteers.fetch("name")
+  #     id = volunteers.fetch("id").to_i
+  #     volunteers.push(Album.new({:name => name, :id => id}))
+  #   end
+  #   volunteers
+  # end
+
+  # def save
+  #   result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+  #   @id = result.first().fetch("id").to_i
+  # end  
 end
+
